08f6c389f7639e15a168e1002d05db28b17a6fa9
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-08-01 20:14:44 +0000

    + Добавлена функция error404(), при вызове которой двиг сгенерирует ошибку "страница не найдена" (и в заголовках отдаёт ошибку "404 Not found"). Тип вывода ошибки зависит от настройки значения "настройки системы" => "основные настройки" => "реакция на неизвестный URL" % При обращении к несуществующей статической странице вызывается функция error404(), это позволит не замусоривать поисковики несуществующими в реальности статическими страницами.

--- ngcms/trunk/engine/includes/inc/functions.inc.php
+++ ngcms/trunk/engine/includes/inc/functions.inc.php
@@ -1454,3 +1454,34 @@ function printHTTPheaders() {
 		@header($hkey.': '.$hvalue);
 	}
 }
+
+
+//
+// Generate error "PAGE NOT FOUND"
+//
+function error404(){
+	global $config, $tpl, $template, $SYSTEM_FLAGS;
+
+	@header('HTTP/1.1 404 Not found');
+	switch ($config['404_mode']) {
+		// HTTP error 404
+		case 2:
+			exit;
+
+			// External error template
+		case 1:
+			$tpl->template('404.external', tpl_site);
+			$tpl->vars('404.external', array());
+			echo $tpl->show('404.external');
+			exit;
+
+			// Internal error template
+		case 0:
+		default:
+			$tpl->template('404.internal', tpl_site);
+			$tpl->vars('404.internal', array());
+			$template['vars']['mainblock'] = $tpl->show('404.internal');
+
+			$SYSTEM_FLAGS['info']['title']['group']	= $lang['404.title'];
+	}
+}
