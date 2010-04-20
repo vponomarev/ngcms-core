d4b2beaf0f2e191c8d1d3c16756dbc5701b5f331
Amarelius <Amarelius@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-04-20 09:08:49 +0000

    + Добавлена функция imagecopymerge_alpha, теперь поддерживается наложение прозрачных штамп-картинок в формате png с активным alpha-каналом

--- ngcms/trunk/engine/includes/classes/upload.class.php
+++ ngcms/trunk/engine/includes/classes/upload.class.php
@@ -799,7 +799,10 @@ class image_managment{
 				$param['stamp_transparency'] = 40;
 			}
 
-			imageCopyMerge($img, $stamp, $destX, $destY, 0, 0, $stampX, $stampY, $param['stamp_transparency']);
+			if ($stampType == 3)
+				$this->imagecopymerge_alpha($img, $stamp, $destX, $destY, 0, 0, $stampX, $stampY, $param['stamp_transparency']);
+			else
+				imageCopyMerge($img, $stamp, $destX, $destY, 0, 0, $stampX, $stampY, $param['stamp_transparency']);
 		}
 
 
@@ -848,4 +851,46 @@ class image_managment{
 		}
 		return 1;
 	}
+	function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct){
+		if(!isset($pct)){
+			return false;
+		}
+		$pct /= 100;
+		// Get image width and height
+		$w = imagesx( $src_im );
+		$h = imagesy( $src_im );
+		// Turn alpha blending off
+		imagealphablending( $src_im, false );
+		// Find the most opaque pixel in the image (the one with the smallest alpha value)
+		$minalpha = 127;
+		for( $x = 0; $x < $w; $x++ )
+		for( $y = 0; $y < $h; $y++ ){
+			$alpha = ( imagecolorat( $src_im, $x, $y ) >> 24 ) & 0xFF;
+			if( $alpha < $minalpha ){
+				$minalpha = $alpha;
+			}
+		}
+		//loop through image pixels and modify alpha for each
+		for( $x = 0; $x < $w; $x++ ){
+			for( $y = 0; $y < $h; $y++ ){
+				//get current alpha value (represents the TANSPARENCY!)
+				$colorxy = imagecolorat( $src_im, $x, $y );
+				$alpha = ( $colorxy >> 24 ) & 0xFF;
+				//calculate new alpha
+				if( $minalpha !== 127 ){
+					$alpha = 127 + 127 * $pct * ( $alpha - 127 ) / ( 127 - $minalpha );
+				} else {
+					$alpha += 127 * $pct;
+				}
+				//get the color index with new alpha
+				$alphacolorxy = imagecolorallocatealpha( $src_im, ( $colorxy >> 16 ) & 0xFF, ( $colorxy >> 8 ) & 0xFF, $colorxy & 0xFF, $alpha );
+				//set pixel with the new color + opacity
+				if( !imagesetpixel( $src_im, $x, $y, $alphacolorxy ) ){
+					return false;
+				}
+			}
+		}
+		// The image copy
+		imagecopy($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h);
+	} 
 }
