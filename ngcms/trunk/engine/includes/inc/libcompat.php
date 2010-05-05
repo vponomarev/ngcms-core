533a133973e1dfbb7f4bce5a0454d12de9bd3ce0
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-05-05 20:52:50 +0000

    + Выложена библиотека обратной совместимости для НЕ-ЧПУ сайтов. При активации библиотеки (ручной активации!) пользователь, зашедший на ссылку в не-ЧПУ формате (к примеру, из поисковой системы) будет автоматически переадресовываться на новую ссылку в ЧПУ формате.
    Метод активации - открываем файл engine/core.php на редактирование, находим строку ~197 и раскомментируем (т.е. удаляем в начале символы '//') строки:
    //include_once root.'includes/inc/libcompat.php';
    //compatRedirector();

---
+++ ngcms/trunk/engine/includes/inc/libcompat.php
@@ -0,0 +1,77 @@
+<?php
+
+//
+// Библиотека для сохранения обратной совместимости при работе не-ЧПУ сайтов
+//
+
+function compatRedirector() {
+	global $mysql, $catz;
+	$uri = $_SERVER['REQUEST_URI'];
+
+	//print "<pre>".var_export($_SERVER, true)."</pre>";
+
+	if (preg_match('#^\/\?#', $uri, $null)) {
+		// Наш клиент
+		//print "GET PARAMS:<br/>\n<pre>".var_export($_GET, true)."</pre>";
+
+		if (isset($_GET['category']) && isset($_GET['altname'])) {
+			// Полная новость, находим её
+			if ($nrow = $mysql->record("select * from ".prefix."_news where alt_name=".db_squote($_GET['altname']))) {
+				$link = newGenerateLink($nrow, false, 0, true);
+				//print "Redirect: ".$link;
+				header("Location: ".$link);
+			} else {
+				//print "Unknown news";
+				header("Location: /");
+			}
+			exit;
+		} else if (isset($_GET['category'])) {
+			// Страница категории
+			if (isset($catz[$_GET['category']])) {
+				$xc = $catz[$_GET['category']];
+				$params = array('category' => $xc['alt'], 'catid' => $xc['id']);
+				if (isset($_GET['cstart']))
+					$params['page'] = intval($_GET['cstart']);
+
+				$link = generateLink('news', 'by.category', $params);
+				//print "Redirect: ".$link;
+				header("Location: ".$link);
+			} else {
+				//print "Unknown category";
+				header("Location: /");
+			}
+			exit;
+		} else if (isset($_GET['year'])) {
+			// Адресация по дате [год]
+			if (isset($_GET['month']) && isset($_GET['day'])) {
+				$params = array('year' => sprintf('%04u', intval($_GET['year'])), 'month' => sprintf('%02u', intval($_GET['month'])), 'day' => sprintf('%02u', intval($_GET['day'])));
+				if (isset($_GET['cstart']))
+					$params['page'] = intval($_GET['cstart']);
+
+				$link = generateLink('news', 'by.day', $params);
+				//print "Redirect: ".$link;
+				header("Location: ".$link);
+				exit;
+			}
+			if (isset($_GET['month'])) {
+				$params = array('year' => sprintf('%04u', intval($_GET['year'])), 'month' => sprintf('%02u', intval($_GET['month'])));
+				if (isset($_GET['cstart']))
+					$params['page'] = intval($_GET['cstart']);
+
+				$link = generateLink('news', 'by.month', $params);
+				//print "Redirect: ".$link;
+				header("Location: ".$link);
+				exit;
+			}
+
+			$params = array('year' => sprintf('%04u', intval($_GET['year'])));
+			if (isset($_GET['cstart']))
+				$params['page'] = intval($_GET['cstart']);
+
+			$link = generateLink('news', 'by.year', $params);
+			//print "Redirect: ".$link;
+			header("Location: ".$link);
+			exit;
+		}
+	}
+}
