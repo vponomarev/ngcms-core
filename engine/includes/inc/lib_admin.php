40292177363eaa79cf776f4ff8f1ac18b6a618dd
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-01-12 23:04:53 +0000

    % Минорная оптимизация кода

--- engine/includes/inc/lib_admin.php
+++ engine/includes/inc/lib_admin.php
@@ -3,7 +3,7 @@
 //
 // Copyright (C) 2006-2012 Next Generation CMS (http://ngcms.ru/)
 // Name: lib_admin.php
-// Description: Libraries for admin panel
+// Description: General function for site administration calls
 // Author: Vitaly Ponomarev
 //
 
@@ -244,6 +244,257 @@ function dbBackup($fname, $gzmode, $tlist = ''){
 	}
 	if ($gzmode)	gzclose($fh);
 	else			fclose($fh);
+
+	return 1;
+}
+
+
+// ======================================================================================================
+// Add news
+// ======================================================================================================
+// $mode - calling mode
+//	* 	'onsite'	- flag if we're called in `onsite` mode (adding news on site)
+function addNews($mode = array()){
+	global $mysql, $lang, $userROW, $parse, $PFILTERS, $config, $catz, $catmap;
+
+	// Set default calling params
+	if (!isset($mode['onsite'])) {
+		$mode['onsite'] = false;
+	}
+
+	// Load permissions
+	$perm = checkPermission(array('plugin' => '#admin', 'item' => 'news'), null, array(
+		'add',
+		'add.onsite',
+		'add.approve',
+		'add.mainpage',
+		'add.pinned',
+		'add.favorite',
+		'add.html',
+		'personal.view',
+		'personal.modify',
+		'personal.modify.published',
+		'personal.publish',
+		'personal.unpublish',
+		'personal.delete',
+		'personal.delete.published',
+		'personal.html',
+		'personal.mainpage',
+		'personal.pinned',
+		'personal.favorite',
+		'personal.setviews',
+		'personal.multicat',
+		'personal.customdate',
+	));
+
+
+	// Check permissions
+	if (!$perm['add'.($mode['onsite']?'.onsite':'')]) {
+		msg(array("type" => "error", "text" => $lang['perm.denied']));
+		return 0;
+	}
+
+
+	$title = $_REQUEST['title'];
+
+	// Fill content
+	$content	= '';
+
+	// Check if EDITOR SPLIT feature is activated
+	if ($config['news.edit.split']) {
+		// Prepare delimiter
+		$ed = '<!--more-->';
+		if ($config['extended_more'] && ($_REQUEST['content_delimiter'] != '')) {
+			// Disable `new line` + protect from XSS
+			$ed = '<!--more="'.str_replace(array("\r", "\n", '"'), '', $_REQUEST['content_delimiter']).'"-->';
+		}
+		$content = $_REQUEST['ng_news_content_short'].(($_REQUEST['ng_news_content_full'] != '')?$ed.$_REQUEST['ng_news_content_full']:'');
+
+	} else {
+		$content = $_REQUEST['ng_news_content'];
+	}
+
+	// Rewrite `\r\n` to `\n`
+	$content = str_replace("\r\n", "\n", $content);
+
+	$alt_name = $parse->translit(trim($_REQUEST['alt_name']), 1);
+
+	// Check title
+	if ((!strlen(trim($title))) || ((!strlen(trim($content))) && (!$config['news_without_content']))) {
+		msg(array("type" => "error", "text" => $lang['addnews']['msge_fields'], "info" => $lang['addnews']['msgi_fields']));
+		return 0;
+	}
+
+	$SQL['title'] = $title;
+
+	// Check for dup if alt_name is specified [ alt name can't be specified during `onsite` add ]
+	if ($alt_name && (!$mode['onsite'])) {
+		if ( is_array($mysql->record("select id from ".prefix."_news where alt_name = ".db_squote($alt_name)." limit 1")) ) {
+			msg(array("type" => "error", "text" => $lang['addnews']['msge_alt_name'], "info" => $lang['addnews']['msgi_alt_name']));
+			return 0;
+		}
+		$SQL['alt_name'] = $alt_name;
+	} else {
+		// Generate uniq alt_name if no alt_name specified
+		$alt_name = strtolower($parse->translit(trim($title), 1));
+		// Make a conversion:
+		// * '.'  to '_'
+		// * '__' to '_' (several to one)
+		// * Delete leading/finishing '_'
+		$alt_name = preg_replace(array('/\./', '/(_{2,20})/', '/^(_+)/', '/(_+)$/'), array('_', '_'), $alt_name);
+
+		// Make alt_name equal to '_' if it appear to be blank after conversion
+		if ($alt_name == '') $alt_name = '_';
+
+		$i = '';
+		while ( is_array($mysql->record("select id from ".prefix."_news where alt_name = ".db_squote($alt_name.$i)." limit 1")) ) {
+			$i++;
+		}
+		$SQL['alt_name'] = $alt_name.$i;
+	}
+
+	// Custom date[ only while adding via admin panel ]
+	if ($_REQUEST['customdate'] && $perm['personal.customdate'] && (!$mode['onsite'])) {
+		$SQL['postdate'] = mktime(intval($_REQUEST['c_hour']), intval($_REQUEST['c_minute']), 0, intval($_REQUEST['c_month']), intval($_REQUEST['c_day']), intval($_REQUEST['c_year'])) + ($config['date_adjust'] * 60);
+	} else {
+		$SQL['postdate'] = time() + ($config['date_adjust'] * 60);
+	}
+
+	$SQL['editdate'] = $SQL['postdate'];
+
+	// Fetch MASTER provided categories
+	$catids = array ();
+	if (intval($_POST['category']) && isset($catmap[intval($_POST['category'])])) {
+		$catids[intval($_POST['category'])] = 1;
+	}
+
+	// Fetch ADDITIONAL provided categories [if allowed]
+	if ($perm['personal.multicat']) {
+		foreach ($_POST as $k => $v) {
+			if (preg_match('#^category_(\d+)$#', $k, $match) && $v && isset($catmap[intval($match[1])]))
+				$catids[$match[1]] = 1;
+		}
+	}
+
+	// Metatags (only for adding via admin panel)
+	if ($config['meta'] && (!$mode['onsite'])) {
+		$SQL['description']	= $_REQUEST['description'];
+		$SQL['keywords']	= $_REQUEST['keywords'];
+	}
+
+	$SQL['author']		= $userROW['name'];
+	$SQL['author_id']	= $userROW['id'];
+	$SQL['catid']		= implode(",", array_keys($catids));
+
+	// Variable FLAGS is a bit-variable:
+	// 0 = RAW mode		[if set, no conversion "\n" => "<br />" will be done]
+	// 1 = HTML enable	[if set, HTML codes may be used in news]
+
+	if ($perm['personal.html']) {
+		$SQL['flags']	=	($_REQUEST['flag_RAW']?1:0) + ($_REQUEST['flag_HTML']?2:0);
+	} else {
+		$SQL['flags']	=	0;
+	}
+
+	// This functions are not available in onsite mode
+	if (!$mode['onsite']) {
+		$SQL['mainpage']	= intval($_REQUEST['mainpage']) && $perm['personal.mainpage'];
+		$SQL['favorite']	= intval($_REQUEST['favorite']) && $perm['personal.favorite'];
+		$SQL['pinned']		= intval($_REQUEST['pinned']) && $perm['personal.pinned'];
+	}
+
+	switch (intval($_REQUEST['approve'])) {
+		case -1:	$SQL['approve'] = -1;								break;
+		case 0:		$SQL['approve'] = 0;								break;
+		case 1:		$SQL['approve'] = $perm['personal.publish']?1:0;	break;
+		default:	$SQL['approve']	= 0;
+	}
+
+	$SQL['content']		= $content;
+
+	exec_acts('addnews');
+
+	$pluginNoError = 1;
+	if (is_array($PFILTERS['news']))
+		foreach ($PFILTERS['news'] as $k => $v) {
+			if (!($pluginNoError = $v->addNews($tvars, $SQL))) {
+				msg(array("type" => "error", "text" => str_replace('{plugin}', $k, $lang['addnews']['msge_pluginlock'])));
+				break;
+			}
+		}
+
+	if (!$pluginNoError) {
+		return 0;
+	}
+
+	$vnames = array(); $vparams = array();
+	foreach ($SQL as $k => $v) { $vnames[]  = $k; $vparams[] = db_squote($v); }
+
+	$mysql->query("insert into ".prefix."_news (".implode(",",$vnames).") values (".implode(",",$vparams).")");
+	$id = $mysql->result("SELECT LAST_INSERT_ID() as id");
+
+	// Update category / user posts counter [ ONLY if news is approved ]
+	if ($SQL['approve'] == 1) {
+		if (count($catids)) {
+			$mysql->query("update ".prefix."_category set posts=posts+1 where id in (".implode(", ",array_keys($catids)).")");
+			foreach (array_keys($catids) as $catid) {
+				$mysql->query("insert into ".prefix."_news_map (newsID, categoryID) values (".db_squote($id).", ".db_squote($catid).")");
+			}
+		}
+		$mysql->query("update ".uprefix."_users set news=news+1 where id=".$SQL['author_id']);
+	}
+
+	// Attaches are available only for admin panel
+	if (!$mode['onsite']) {
+
+		// Now let's manage attached files
+		$fmanager = new file_managment();
+
+		$flagUpdateAttachCount = false;
+
+		// Delete files (if needed)
+		foreach ($_POST as $k => $v) {
+			if (preg_match('#^delfile_(\d+)$#', $k, $match)) {
+				$fmanager->file_delete(array('type' => 'file', 'id' => $match[1]));
+				$flagUpdateAttachCount = true;
+			}
+		}
+
+		//print "<pre>".var_export($_FILES, true)."</pre>";
+		// PREPARE a list for upload
+		if (is_array($_FILES['userfile']['name']))
+			foreach($_FILES['userfile']['name'] as $i => $v) {
+				if ($v == '')
+					continue;
+
+				$flagUpdateAttachCount = true;
+				//
+				$up = $fmanager->file_upload(array('dsn' => true, 'linked_ds' => 1, 'linked_id' => $id, 'type' => 'file', 'http_var' => 'userfile', 'http_varnum' => $i));
+				//print "OUT: <pre>".var_export($up, true)."</pre>";
+				if (!is_array($up)) {
+					// Error uploading file
+					// ... show error message ...
+				}
+			}
+
+		// Update attach count if we need this
+		$numFiles = $mysql->result("select count(*) as cnt from ".prefix."_files where (storage=1) and (linked_ds=1) and (linked_id=".db_squote($id).")");
+		if ($numFiles) {
+			$mysql->query("update ".prefix."_news set num_files = ".intval($numFiles)." where id = ".db_squote($id));
+		}
+
+		$numImages = $mysql->result("select count(*) as cnt from ".prefix."_images where (storage=1) and (linked_ds=1) and (linked_id=".db_squote($id).")");
+		if ($numImages) {
+			$mysql->query("update ".prefix."_news set num_images = ".intval($numImages)." where id = ".db_squote($id));
+		}
+	}
+
+	// Notify plugins about adding new news
+	if (is_array($PFILTERS['news']))
+		foreach ($PFILTERS['news'] as $k => $v) { $v->addNewsNotify($tvars, $SQL, $id); }
+
+	exec_acts('addnews_', $id);
+	msg(array("text" => $lang['addnews']['msgo_added'], "info" => sprintf($lang['addnews']['msgi_added'], admin_url.'/admin.php?mod=news&action=edit&id='.$id, admin_url.'/admin.php?mod=news')));
 
 	return 1;
 }
