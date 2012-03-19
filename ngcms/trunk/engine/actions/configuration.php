f9a0897f7249cf8a282baeca92bedd43534bf39d
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-03-19 20:41:52 +0000

    % В настройках системы добавлена возможность задавать Имя и EMail отправителя от которых двиг будет рассылать письма

--- ngcms/trunk/engine/actions/configuration.php
+++ ngcms/trunk/engine/actions/configuration.php
@@ -1,7 +1,7 @@
 <?php
 
 //
-// Copyright (C) 2006-2011 Next Generation CMS (http://ngcms.ru/)
+// Copyright (C) 2006-2012 Next Generation CMS (http://ngcms.ru/)
 // Name: configuration.php
 // Description: Configuration managment
 // Author: Vitaly Ponomarev, Alexey Zinchenko
