c6a6f0e63c27d4483ecb576a3631a82f88a8d87b
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-10-06 23:06:45 +0000

    % Обновлён маркер версии + Для плагинов теперь доступна функция registerUser (в классе CoreFilter), она вызывается до фактической регистрации пользователя и может блокировать регистрацию.

--- ngcms/trunk/engine/includes/inc/consts.inc.php
+++ ngcms/trunk/engine/includes/inc/consts.inc.php
@@ -17,7 +17,7 @@
 @define('NGCMS', true);
 
 @define('engineName', 'NGCMS');
-@define('engineVersion', '0.9.3 + SVN [SVN1068+]');
+@define('engineVersion', '0.9.3 + SVN [SVN1070+]');
 
 @define('prefix', $config['prefix']);
 @define('home', $config['home_url']);
