ade35ac66affd0f6979c39e8f103e37f7c8b428d
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-10-06 22:22:08 +0000

    + Добавлена новая TWIG функция isNews(), позволяет делать условные блоки, работающие в конкретных новостях или в новостях из конкретных категорий. Детальное описание функции лежит в readme/docs/ % Обновлён маркер версии

--- engine/includes/inc/consts.inc.php
+++ engine/includes/inc/consts.inc.php
@@ -17,7 +17,7 @@
 @define('NGCMS', true);
 
 @define('engineName', 'NGCMS');
-@define('engineVersion', '0.9.3 + SVN [SVN1062+]');
+@define('engineVersion', '0.9.3 + SVN [SVN1068+]');
 
 @define('prefix', $config['prefix']);
 @define('home', $config['home_url']);
