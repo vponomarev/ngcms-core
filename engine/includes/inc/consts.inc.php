b7dce4a363b8d444b15afcfc9e7adfd249c64ba7
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-10-18 19:49:19 +0000

    % Убран ставший ненужным код из модуля управления шаблонами

--- engine/includes/inc/consts.inc.php
+++ engine/includes/inc/consts.inc.php
@@ -17,7 +17,7 @@
 @define('NGCMS', true);
 
 @define('engineName', 'NGCMS');
-@define('engineVersion', '0.9.3 + SVN [SVN1086+]');
+@define('engineVersion', '0.9.3 + SVN [SVN1088+]');
 
 @define('prefix', $config['prefix']);
 @define('home', $config['home_url']);
