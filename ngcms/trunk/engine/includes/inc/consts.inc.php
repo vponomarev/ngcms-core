7a11682f3fe76794750a732473b5755f03300cb6
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-11-22 22:20:42 +0000

    + Добавлена возможность использования TWIG функции template_from_string(), она позволяет исполнять TWIG код, находящийся в переменной. Пример использования: {% include template_from_string(category.info) %} В данном примере мы исполняем TWIG код из переменной category.info

--- ngcms/trunk/engine/includes/inc/consts.inc.php
+++ ngcms/trunk/engine/includes/inc/consts.inc.php
@@ -17,7 +17,7 @@
 @define('NGCMS', true);
 
 @define('engineName', 'NGCMS');
-@define('engineVersion', '0.9.3 + SVN [SVN1101+]');
+@define('engineVersion', '0.9.3 + SVN [SVN1102+]');
 
 @define('prefix', $config['prefix']);
 @define('home', $config['home_url']);
