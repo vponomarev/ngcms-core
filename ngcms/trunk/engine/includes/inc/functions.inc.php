0dd9a3aa3b1d61acacb663ac8da275d3f34625cd
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2013-01-11 17:42:50 +0000

    % Исправлена ошибка в генерации шаблона news.categories.tpl (необязательный шаблон), обновлена документация по шаблону

--- ngcms/trunk/engine/includes/inc/functions.inc.php
+++ ngcms/trunk/engine/includes/inc/functions.inc.php
@@ -1106,7 +1106,6 @@ function generateCategoryMenu(){
 
 				// Mark all levels that are closed after this item
 				if ($i == (count($tEntries)-1)) {
-				//if ($i == 26) {
 					for ($x = 0; $x <= $tEntries[$i]['level']; $x++) {
 						$tEntries[$i]['flags']['closeLevel_'.$x] = true;
 					}
@@ -1115,7 +1114,8 @@ function generateCategoryMenu(){
 						$tEntries[$i]['flags']['closeLevel_'.$x] = true;
 					}
 				}
-				$tEntries[$i]['flags']['closeToLevel'] = intval($tEntries[$i+1]['level']);
+				if ($tEntries[$i]['level'] > $tEntries[$i+1]['level'])
+					$tEntries[$i]['closeToLevel'] = intval($tEntries[$i+1]['level']);
 			}
 
 		}
@@ -2652,4 +2652,56 @@ function twigTruncateHTML($string, $len = 70, $finisher = '') {
 	global $parse;
 
 	return $parse->truncateHTML($string, $len, $finisher);
+}
+
+
+function jsonFormatter($json) {
+
+	$result = '';
+	$pos = 0;
+	$strLen = strlen($json);
+	$indentStr = '  ';
+	$newLine = "\n";
+	$prevChar = '';
+	$outOfQuotes = true;
+
+	for ($i = 0; $i <= $strLen; $i++) {
+
+		// Grab the next character in the string.
+		$char = substr($json, $i, 1);
+
+		// Are we inside a quoted string?
+		if ($char == '"' && $prevChar != '\\') {
+			$outOfQuotes = !$outOfQuotes;
+
+			// If this character is the end of an element,
+			// output a new line and indent the next line.
+		} else if (($char == '}' || $char == ']') && $outOfQuotes) {
+			$result .= $newLine;
+			$pos--;
+			for ($j = 0; $j < $pos; $j++) {
+				$result .= $indentStr;
+			}
+		}
+
+		// Add the character to the result string.
+		$result .= $char;
+
+		// If the last character was the beginning of an element,
+		// output a new line and indent the next line.
+		if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+			$result .= $newLine;
+			if ($char == '{' || $char == '[') {
+				$pos++;
+			}
+
+			for ($j = 0; $j < $pos; $j++) {
+				$result .= $indentStr;
+			}
+		}
+
+		$prevChar = $char;
+	}
+
+	return $result;
 }
