7b1e5bbeb0287f524795a0180d9cbde7ab729ce7
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2013-01-18 20:06:01 +0000

    % Исправлена ошибка - не работал модуль suggest (подсказка), была проблема в CSS стилях.

--- ngcms/trunk/engine/includes/inc/consts.inc.php
+++ ngcms/trunk/engine/includes/inc/consts.inc.php
@@ -17,7 +17,7 @@
 @define('NGCMS', true);
 
 @define('engineName', 'NGCMS');
-@define('engineVersion', '0.9.3 + SVN [SVN1121+]');
+@define('engineVersion', '0.9.3 + SVN [SVN1122+]');
 
 @define('prefix', $config['prefix']);
 @define('home', $config['home_url']);
