00ebed7f78d4d774f0f9f6af3eaacb9d9525a3ed
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2013-01-20 20:07:11 +0000

    % Возвращено сообщение "к сожалению новостей нет" при заходе в категорию, в которой нет новостей.

--- ngcms/trunk/engine/core.php
+++ ngcms/trunk/engine/core.php
@@ -267,6 +267,7 @@ $twig->addFunction('isSet',			new Twig_Function_Function('twigIsSet', array('nee
 $twig->addFunction('debugContext',	new Twig_Function_Function('twigDebugContext', array('needs_context' => true)));
 $twig->addFunction('debugValue',	new Twig_Function_Function('twigDebugValue'));
 $twig->addFunction('getCategoryTree', 	new Twig_Function_Function('twigGetCategoryTree'));
+$twig->addFunction('engineMSG', 	new Twig_Function_Function('twigEngineMSG'));
 
 $twig->addFilter('truncateHTML',	new Twig_Filter_Function('twigTruncateHTML'));
 
--- ngcms/trunk/templates/default/news.table.tpl
+++ ngcms/trunk/templates/default/news.table.tpl
@@ -1,1 +1,11 @@
-{% for entry in data %}{{ entry }}{% endfor %} {{ pagination }}
+{% if (handler == 'by.category') %}
+<h3>{{ category.name }}</h3>
+{% include template_from_string(category.info) %}
+{% if category.icon.purl %}<img src="{{ category.icon.purl }}"/><br/>{% endif %}
+{% endif %}
+{% for entry in data %}
+{{ entry }}
+{% else %}
+{{ engineMSG('commin', lang['msgi_no_news']) }}
+{% endfor %}
+{{ pagination }}
