8bbe45edcfead64899579b18a29d9852a7076a88
stdex <stdex@e0e46c49-be69-4f5a-ad62-21024a331aea>
2013-10-30 04:24:07 +0000

    % Обновлён маркер версии

--- engine/includes/inc/consts.inc.php
+++ engine/includes/inc/consts.inc.php
@@ -17,7 +17,7 @@
 @define('NGCMS', true);
 
 @define('engineName', 'NGCMS');
-@define('engineVersion', '0.9.3 + SVN [SVN1178+]');
+@define('engineVersion', '0.9.3 + SVN [SVN1226+]');
 
 @define('prefix', $config['prefix']);
 @define('uprefix',$config['uprefix']);
