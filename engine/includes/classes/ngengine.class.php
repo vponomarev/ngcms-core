fef94cb34f69078c8ebd717b1c09a32f834a4015
Vitaly <vitaly.ponomarev@gmail.com>
2017-03-06 16:13:57 +0300

    Publish basic classes.

--- engine/includes/classes/ngengine.class.php
+++ engine/includes/classes/ngengine.class.php
@@ -5,6 +5,10 @@
 class NGEngine {
     protected static $instance = false;
     protected static $items = array();
+
+    /**
+     * @return NGEngine Return instance of NGEngine singleton
+     */
     static function getInstance() {
         if (NGEngine::$instance === false) {
             NGEngine::$instance = new static();
@@ -29,12 +33,34 @@ class NGEngine {
 
     // Get DB connection
     function getDB() {
+        if (!isset(NGEngine::$items['db']))
+            throw new Exception('NGEngine: getDB(): DB class is not initalized.');
+
         return NGEngine::$items['db'];
     }
 
     // Get Legacy DB connection
     function getLegacyDB() {
+        if (!isset(NGEngine::$items['legacyDB']))
+            throw new Exception('NGEngine: getLegacyDB(): DB class is not initalized.');
+
         return NGEngine::$items['legacyDB'];
+    }
+
+    // Get event handler
+    function getEvents() {
+        if (!isset(NGEngine::$items['events']))
+            throw new Exception('NGEngine: getEvents(): Event handler is not loaded.');
+
+        return NGEngine::$items['events'];
+    }
+
+    // Get error handler
+    function getErrorHandler() {
+        if (!isset(NGEngine::$items['errorHandler']))
+            throw new Exception('NGEngine: getErrorHandler(): Error handler is not loaded.');
+
+        return NGEngine::$items['errorHandler'];
     }
 
     // Get configurarion parameter
@@ -53,6 +79,9 @@ class NGEngine {
         return isset(NGEngine::$items['currentUser'])?true:false;
     }
 
+    /**
+     * @return NGUser Return instance of NGUser for current user
+     */
     function getCurrentUser() {
         return isset(NGEngine::$items['currentUser'])?NGEngine::$items['currentUser']:null;
     }
---
+++ engine/includes/classes/ngevents.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class NGEvents {
+    protected $eventList = array();
+    protected $startTime;
+
+    function __construct() {
+        // Init event processing
+        $this->startTime = microtime(true);
+    }
+
+    // Register event
+    function registerEvent($group, $plugin = null, $info = null, $duration = null) {
+        $timeStamp = round(microtime(true) - $this->startTime, 2);
+        if (is_array($group)) {
+            $group['timestamp'] = $timeStamp;
+            $this->eventList []= $group;
+        } else {
+            $this->eventList []= array(
+                'timestamp'     => $timeStamp,
+                'plugin'        => $plugin,
+                'info'          => $info,
+                'duration'      => $duration,
+            );
+        }
+    }
+
+    public function getEventList() {
+        return $this->eventList;
+    }
+
+    public function tickStart() {
+        return microtime(true);
+    }
+
+    public function tickStop($t) {
+        return round(microtime(true) - $t, 2);
+    }
+
+}
