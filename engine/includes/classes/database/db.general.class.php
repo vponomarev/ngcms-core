537152f3d15a363085e4bf723f3d368266fc63f0
Vitaly <vitaly.ponomarev@gmail.com>
2017-03-06 13:50:26 +0300

    Publish basic abstract class for DB interaction

---
+++ engine/includes/classes/database/db.general.class.php
@@ -0,0 +1,25 @@
+<?php
+
+abstract class NG_DB {
+    // Implement connection to DB
+    abstract function __construct($params);
+
+    // Generate select request to DB [ ROWS are returned ]
+    abstract function select($query, $params = array());
+
+    // Generate SQL request and return SINGLE line [ ROWS are returned ]
+    abstract function record($query, $params = array());
+
+    // Generate SQL request and return SINGLE line
+    abstract function exec($query, $params = array());
+
+    // Generate SQL request and return FIRST variable from SINGLE line
+    abstract function result($query, $params = array());
+
+    // Return engine type
+    abstract function getEngineType();
+
+    // Return raw class of DB engine driver
+    abstract function getDriver();
+
+}
