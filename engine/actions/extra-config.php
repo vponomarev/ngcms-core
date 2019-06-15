ebe1baf3cc524b6b1ac023e14a0faf1a7aa7bf70
Megaket4up <megaket4up@gmail.com>
2019-06-15 19:47:28 +0500

    Fix preinstall plugin page

--- engine/actions/extra-config.php
+++ engine/actions/extra-config.php
@@ -65,7 +65,7 @@ if (!is_array($extras[$plugin])) {
         // Include required script file
         ob_start();
         @include extras_dir . '/' . $extras[$plugin]['dir'] . '/' . $extras[$plugin][$stype];
-        $main_admin = ob_get_contents();
+        $main_admin .= ob_get_contents();
         ob_end_clean();
 
         //
