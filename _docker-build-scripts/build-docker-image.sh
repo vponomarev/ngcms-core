b336c9aeb69bc38850ec63d673811a8d7550fe83
Vitaly A Ponomarev <vitaly.ponomarev@gmail.com>
2020-06-25 11:59:07 +0300

    Tune docker build scripts.

--- _docker-build-scripts/build-docker-image.sh
+++ _docker-build-scripts/build-docker-image.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Remove old build
-rmdir -f build
+rm -rf build/
 
 # Create new build dir and download sources
 mkdir build
 git clone https://github.com/vponomarev/ngcms-core build/
-git clone https://github.com/vponomarev/ngcms-plugins build/engine/plugins/
+git clone -b transfer_utf8 https://github.com/vponomarev/ngcms-plugins build/engine/plugins/
 rm build.tgz
 cd build
 tar czf ../build.tgz .
